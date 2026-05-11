4d6c75a2a2928c6d8e6865172658214b1e001100
NAJEM-BRAHIM <SAHARA.INFO.SERVICES@GMAIL.COM>
2026-05-11 12:25:49 +0000

    [FIX] Restore manifest with correct data order

--- cumarex_hr_payroll_damancom/__manifest__.py
+++ cumarex_hr_payroll_damancom/__manifest__.py
@@ -1,33 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Cumarex - Télédéclaration CNSS Damancom',
+    'name': 'Cumarex - Teledeclaration CNSS Damancom',
     'version': '19.0.1.0.0',
     'category': 'Human Resources/Payroll',
-    'summary': "Génération du fichier BDS pour Damancom CNSS Maroc",
+    'summary': "Generation du fichier BDS pour Damancom CNSS Maroc",
     'description': """
-Module de Télédéclaration CNSS Damancom pour le Maroc
+Module de Teledeclaration CNSS Damancom pour le Maroc
 ======================================================
 
-Ce module permet de générer le fichier BDS au format officiel CNSS
-(version 2 - Février 2006) pour la télédéclaration mensuelle des salaires
+Ce module permet de generer le fichier BDS au format officiel CNSS
+(version 2 - Fevrier 2006) pour la teledeclaration mensuelle des salaires
 sur le portail Damancom (e-BDS).
-
-Fonctionnalités:
-----------------
-* Configuration du numéro d'affiliation CNSS de l'entreprise (avec validation clé)
-* Configuration du numéro d'immatriculation CNSS des employés (avec validation clé)
-* Gestion des situations spéciales (SO, DE, IT, IL, AT, CS, MS, MP)
-* Génération du fichier BDS principal (déclaration normale)
-* Génération du fichier BDS complémentaire (entrants oubliés)
-* Format conforme à 100% au cahier des charges CNSS Version 2
-* Longueur fixe 260 caractères par enregistrement
-* Calculs SBP/SBNP plafonné/déplafonné automatiques
-* Contrôles horizontaux (S_Ctr) et verticaux
-* Nommage automatique : DS_NNNNNNN_MMAAAA.txt
-
-Le fichier généré peut être :
-* Téléchargé et déposé manuellement sur damancom.ma (mode EDI)
-* Utilisé pour vérification avant dépôt
     """,
     'author': 'Cumarex',
     'website': 'https://www.cumarex.ma',
@@ -38,10 +21,9 @@ Le fichier généré peut être :
         'views/res_company_views.xml',
         'views/hr_employee_views.xml',
         'views/hr_payslip_views.xml',
-        'wizards/damancom_generate_wizard_views.xml
-        wizards/damancom_import_preetabli_wizard_views.xml
-        views/damancom_declaration_views.xml',
         'wizards/damancom_generate_wizard_views.xml',
+        'wizards/damancom_import_preetabli_wizard_views.xml',
+        'views/damancom_declaration_views.xml',
         'views/damancom_menus.xml',
     ],
     'installable': True,
